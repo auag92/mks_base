1a54e52a4d2ccda927ae253cd64e10233765e470
auag92 <apaar92@gmail.com>
2017-07-27 09:32:07 -0400

    corrmaster modified

--- mks_base/CorrMaster.py
+++ mks_base/CorrMaster.py
@@ -1,47 +1,92 @@
 import numpy as np
-import math
 
+from toolz.curried import curry
+from toolz.curried import map
+from toolz.curried import pipe
+
+
+fft = curry(np.fft.fft)  # pylint: disable=invalid-name
+
+ifft = curry(np.fft.ifft)  # pylint: disable=invalid-name
+
+fftn = curry(np.fft.fftn)  # pylint: disable=invalid-name
+
+ifftn = curry(np.fft.ifftn)  # pylint: disable=invalid-name
+
+fftshift = curry(np.fft.fftshift)  # pylint: disable=invalid-name
+
+conj = curry(np.conj)
+
+func = curry(lambda x, y: conj(x) * fftn(y))
+
+@curry
+def return_slice(x_data, cutoff):
+
+    s = np.asarray(x_data.shape).astype(int) // 2 + 1
+
+    if x_data.ndim == 2:
+        return x_data[(s[0] - cutoff):(s[0] + cutoff),
+                      (s[1] - cutoff):(s[1] + cutoff)]
+    elif x_data.ndim ==3:
+        return x_data[(s[0] - cutoff):(s[0] + cutoff),
+                      (s[1] - cutoff):(s[1] + cutoff),
+                      (s[2] - cutoff):(s[2] + cutoff)]
+    else:
+        print('Incorrect Number of Dimensions!')
+
+
+@curry
+def corr_master_(x_data, cutoff, func):
+    return pipe(x_data,
+                fftn,
+                func,
+                ifftn,
+                fftshift,
+                return_slice(cutoff=cutoff))
+
+
+@curry
+def corr_master_auto(cutoff, *args):
+    """
+    Returns auto-corrlation of and input field with itself.
+    """
+
+    return pipe(args[0],
+                fftn,
+                lambda x: conj(x)*x,
+                ifftn,
+                fftshift,
+                return_slice(cutoff=cutoff))
+
+
+@curry
+def corr_master_cross(cutoff, *args):
+    """
+    Returns cross corrlation between two input fields.
+    """
+    return pipe(args[0],
+                fftn,
+                lambda x: conj(x) * fftn(args[1]),
+                ifftn,
+                fftshift,
+                return_slice(cutoff = cutoff))
+
+
+# @curry
+# def corr_master_cross(cutoff, *args):
+#     return pipe(args[0],
+#                corr_master_(cutoff=cutoff, func=func(y=args[1])))
+
+
+@curry
 def corr_master(corrtype, cutoff, *args):
-
-    nargin = len(args)
+    """
+    Wrapper function that returns auto or crosscorrelations for
+    input fields by calling appropriate modules.
+    """
     if corrtype == 'auto':
-        H1 = args[0]
-        H1 = np.fft.fftn(H1)
-        H1 = H1*np.conj(H1)
-        H1 = np.fft.fftshift(np.fft.ifftn(H1))
-        GG = H1
-        if H1.ndim == 2:
-            ggx = math.floor(GG.shape[0]/2+1)
-            ggy = math.floor(GG.shape[1]/2+1)
-            GG = GG[(ggx-cutoff):(ggx+cutoff), (ggy-cutoff):(ggy+cutoff)]
-        elif H1.ndim ==3:
-            ggx = math.floor(GG.shape[0]/2+1)
-            ggy = math.floor(GG.shape[1]/2+1)
-            ggz = math.floor(GG.shape[2]/2+1)
-            GG = GG[(ggx-cutoff):(ggx+cutoff), (ggy-cutoff):(ggy+cutoff), (ggz-cutoff):(ggz+cutoff)]
-        else:
-            print('Incorrect Number of Dimensions!')
-        return GG
+        return corr_master_auto(cutoff, *args)
     elif corrtype == 'cross':
-        H1 = args[0]
-        H2 = args[1]
-        H1 = np.fft.fftn(H1)
-        H1 = np.conj(H1)
-        H2 = np.fft.fftn(H2)
-        H1 = H1*H2
-        H1 = np.fft.fftshift(np.fft.ifftn(H1))
-        GG = H1
-        if H1.ndim == 2:
-            ggx = math.floor(GG.shape[0]/2+1)
-            ggy = math.floor(GG.shape[1]/2+1)
-            GG = GG[(ggx-cutoff):(ggx+cutoff), (ggy-cutoff):(ggy+cutoff)]
-        elif H1.ndim ==3:
-            ggx = math.floor(GG.shape[0]/2+1)
-            ggy = math.floor(GG.shape[1]/2+1)
-            ggz = math.floor(GG.shape[2]/2+1)
-            GG = GG[(ggx-cutoff):(ggx+cutoff), (ggy-cutoff):(ggy+cutoff), (ggz-cutoff):(ggz+cutoff)]
-        else:
-            print('Incorrect Number of Dimensions!')
-        return GG
+        return corr_master_cross(cutoff, *args)
     else:
         print("Please either <Auto> or <Cross> as correlation type")
